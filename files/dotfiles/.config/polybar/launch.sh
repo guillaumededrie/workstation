5c24ba7b2f8f14d5214c57f9064ecfd148a15b8d
Guillaume Dedrie <guillaume.dedrie@gmail.com>
2022-10-22 19:41:06 +0200

    feat(screnn): update default Screen layout using Polybar and autorandr

--- files/dotfiles/.config/polybar/launch.sh
+++ files/dotfiles/.config/polybar/launch.sh
@@ -7,7 +7,9 @@ polybar-msg cmd quit
 # killall -q polybar
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar-example.log
-polybar example 2>&1 | tee -a /tmp/polybar-example.log & disown
+echo "---" | tee -a /tmp/polybar.log
+polybar main 2>&1 | tee -a /tmp/polybar-main.log & disown
+polybar left 2>&1 | tee -a /tmp/polybar-left.log & disown
+polybar right 2>&1 | tee -a /tmp/polybar-right.log & disown
 
-echo "Bar launched..."
+echo "Bar(s) launched..."
